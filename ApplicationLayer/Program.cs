693de9d3f4f12779d1b1fda8ec638d039d0c255a
chanthosh <chanthoshsivanandam@gmail.com>
2019-02-28 14:26:47 +0100

    dfgf

--- ApplicationLayer/Program.cs
+++ ApplicationLayer/Program.cs
@@ -11,17 +11,6 @@ namespace ApplicationLayer
 	{
 		static void Main(string[] args)
 		{
-            DatabaseController DBC = new DatabaseController();
-
-            Order order = new Order();
-
-            order.Order_Date =  DateTime.Parse("20.12.2019");
-            order.Order_DeliveryDate = DateTime.Parse("23.12.2019");
-            order.Order_Picked = 1;
-            order.Order_CustomerID = 1;
-
-            DBC.CreateOrder(order);
-
 
         }
 	}
